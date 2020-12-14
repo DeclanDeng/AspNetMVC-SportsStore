d535951e9043216b334b1eb8fd41443f783f610c
declan.deng@outlook.com <declan.deng@outlook.com>
2020-12-14 14:07:40 +1100

    Implement the order processor & Add unit test for order processing

--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Moq;
+using System.Configuration;
 using Ninject;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
@@ -29,6 +29,12 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            ninjectKernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
         }
     }
 }
